e0b114917ee6b79833e4149cb7d73f7cbb8fe92f
NorbertPap <garrote713@gmail.com>
2024-04-09 16:50:09 +0200

    trying pick and place

--- src/arm_handling_node.py
+++ src/arm_handling_node.py
@@ -5,7 +5,9 @@ import actionlib
 import numpy as np
 from moveit_commander import MoveGroupCommander, RobotCommander, PlanningSceneInterface
 import moveit_msgs.msg
-from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import CollisionObject
+from geometry_msgs.msg import PoseStamped, Pose
+from shape_msgs.msg import SolidPrimitive
 from p6.msg import ScanAreaAction, PickAndPlaceAction
 import tf.transformations as tf_trans
 
@@ -29,30 +31,15 @@ class ArmHandlingNode:
 
     def get_cartesian_pose(self):
         arm_group = self.arm_group
-
-        # Get the current pose and display it
         pose = arm_group.get_current_pose()
-        # rospy.loginfo("Actual cartesian pose is : ")
-        # rospy.loginfo(pose.pose)
-
         return pose.pose
 
     def reach_cartesian_pose(self, pose, tolerance = 0, constraints = None):
         arm_group = self.arm_group
-        
-        # Set the tolerance
         arm_group.set_goal_position_tolerance(tolerance)
-
-        # Set the trajectory constraint if one is specified
         if constraints is not None:
             arm_group.set_path_constraints(constraints)
-
-        # Get the current Cartesian Position
         arm_group.set_pose_target(pose)
-
-        # Plan and execute
-        # rospy.loginfo("Planning and going to the Cartesian Pose:")
-        # rospy.loginfo(pose)
         return arm_group.go(wait=True)
     
     def reach_gripper_position(self, relative_position):
@@ -81,9 +68,9 @@ class ArmHandlingNode:
         orientation.z = quat[2]
         orientation.w = quat[3]
 
-        return pose.pose
+        return pose
 
-    def scan_area(self):
+    def scan_area(self, goal = None):
         rospy.loginfo("Scanning area")
         rospy.loginfo("Going to home")
         self.arm_group.set_named_target('home')
@@ -113,23 +100,75 @@ class ArmHandlingNode:
         self.scan_area_server.set_succeeded()
 
     def pick_and_place(self, goal):
-        object_pose = PoseStamped()
-        object_pose.pose.position.x = goal.object.x
-        object_pose.pose.position.y = goal.object.y
-        object_pose.pose.position.z = goal.object.z
+        object_pose = self.get_pose_from_xyz_rpy(goal.object.x, goal.object.y, goal.object.z, 0, 0, 0)
+        place_pose = self.get_pose_from_xyz_rpy(goal.location.x, goal.location.y, goal.location.z + 0.1, 0, 0, 0)
 
-        place_pose = PoseStamped()
-        place_pose.pose.position.x = goal.location.x
-        place_pose.pose.position.y = goal.location.y
-        place_pose.pose.position.z = goal.location.z
+        grasp = moveit_msgs.msg.Grasp()
+        grasp.max_contact_force = 1
+        grasp.grasp_pose = object_pose
+        grasp.grasp_pose.header.frame_id = "base_link"
+        grasp.pre_grasp_approach.direction.header.frame_id = "base_link"
+        grasp.pre_grasp_approach.direction.vector.z = -1.0
+        grasp.pre_grasp_approach.min_distance = 0.15
+        grasp.pre_grasp_approach.desired_distance = 0.2
+        grasp.post_grasp_retreat.direction.header.frame_id = "base_link"
+        grasp.post_grasp_retreat.direction.vector.z = 1.0
+        grasp.post_grasp_retreat.min_distance = 0.15
+        grasp.post_grasp_retreat.desired_distance = 0.2
 
-        # TODO: Use MoveIt's pick and place utility to pick the object and place it at the location
+        rospy.loginfo("Picking object...")
+        self.arm_group.pick('object', grasp)
+        rospy.loginfo("Object picked")
+
+        place = moveit_msgs.msg.PlaceLocation()
+        place.place_pose = place_pose
+        place.pre_place_approach.direction.header.frame_id = "base_link"
+        place.pre_place_approach.direction.vector.z = -1.0
+        place.pre_place_approach.min_distance = 0.15
+        place.pre_place_approach.desired_distance = 0.2
+        place.post_place_retreat.direction.header.frame_id = "base_link"
+        place.post_place_retreat.direction.vector.z = 1.0
+        place.post_place_retreat.min_distance = 0.15
+        place.post_place_retreat.desired_distance = 0.2
+
+        rospy.loginfo("Placing object...")
+        self.arm_group.place('object', place)
+        rospy.loginfo("Object placed")
 
         self.pick_and_place_server.set_succeeded()
 
+def set_scene(arm_handler):
+    # Create a collision object
+    collision_object = CollisionObject()
+    collision_object.header.frame_id = arm_handler.arm_group.get_planning_frame()
+    collision_object.id = 'object'
+
+    # Define the shape and pose of the object
+    primitive = SolidPrimitive()
+    primitive.type = primitive.BOX
+    primitive.dimensions = [0.1, 0.1, 0.1]  # size of the box (1x1x1)
+    collision_object.primitives = [primitive]
+
+    object_pose = Pose()
+    object_pose.orientation.w = 1.0
+    object_pose.position.x = 0.3
+    object_pose.position.y = 0.3
+    object_pose.position.z = 0.3
+    collision_object.primitive_poses = [object_pose]
+
+    arm_handler.scene.add_object(collision_object)
 
 if __name__ == '__main__':
     rospy.init_node('arm_handling_node')
     server = ArmHandlingNode()
-    server.scan_area()
+    set_scene(server)
+    # server.scan_area()
+    action = PickAndPlaceAction()
+    action.action_goal.goal.object.x = 0.3
+    action.action_goal.goal.object.y = 0.3
+    action.action_goal.goal.object.z = 0.3
+    action.action_goal.goal.location.x = 0.4
+    action.action_goal.goal.location.y = 0.4
+    action.action_goal.goal.location.z = 0.3
+    server.pick_and_place(action.action_goal.goal)
     rospy.spin()
